9ed0a5373efabd88f017100f2b87cf20adaddc5c
GAchaitanya <arun.g4201@gmail.com>
2018-09-20 10:45:15 +0530

    updated

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-class employe:
-    '''this is my first script'''
-    def __int__(self,en):
-        self.en=en
-    def info(self):
-        print('emp nme is: ',self.en)
-
-e1=employe('arun')
-e1.info()
+print("Hello World")
